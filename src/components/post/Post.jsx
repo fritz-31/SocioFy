74228a68f0ea62879c5a8d415f9b84c3c61e9533
Ritwik Srivastava <72033393+fritz-31@users.noreply.github.com>
2021-09-21 14:48:28 +0530

    second commit

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -10,7 +10,7 @@ export default function Post() {
                    <div className="postTopLeft">
                        <img className="postProfileImg" src="/assets/person/picture1.jpg" alt=""/>
                    <span className="postUserName"> Ritwik Srivastava</span>
-                   <span className="postDate">5 minutes ago</span>
+                   <span className="postDate"> 5 minutes ago</span>
                    </div>
                    <div className="postTopRight">
                     <MoreVert/>
@@ -19,14 +19,17 @@ export default function Post() {
                </div>
                 <div className="postCenter">
                     <span className="postText">Hey ! its my first post ;)</span>
-                    <img className="postImg" src="/assets/post/picture1.jpg" alt=""/>
+                    <img className="postImg" src="/assets/post/1.jpeg" alt=""/>
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                    <img className="postImg" src="/assets/post/picture1.jpg" alt=""/>  
-                    <img className="postImg" src="/assets/post/picture1.jpg" alt=""/> 
+                    <img className="likeIcon" src="/assets/like.png" alt=""/>  
+                    <img className="likeIcon" src="/assets/heart.png" alt=""/> 
+                    <span className="postLikeCounter">69 people like it</span>
                     </div>
-                    <div className="postBottomRight"></div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">10 comments</span>
+                    </div>
                 </div>
            </div>
         </div>
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,25 @@ import "./rightbar.css"
 export default function Rightbar() {
     return (
         <div className="rightbar">
-           rightbar 
+           <div className="rightbarWrapper">
+               <div className="birthdayContainer">
+                   <img className="birthdayImg" src="assets/gift.png" alt=""/>
+               <span className="birthdayText">
+                   <b>Leanord Hofstader</b> and <b> 3 other friends</b> have a birthday today.
+               </span>
+               </div>
+               <img className="righbarAd" src="assets/ad.png" alt=""/>
+               <h4 className="rightbarTitle">Online Friends</h4>
+               <ul className="rightbarFriendList">
+                   <li className="rightbarFriend">
+                       <div className="rightbarProfileImgContainer">
+                           <img className="righbarProfileImg" src="assets/person/90.jpeg" alt=""/>
+                           <span className="rightbarOnline"></span>
+                       </div>
+                       <span className="rightbarUsername"> Bernedette</span>
+                   </li>
+               </ul>
+           </div>
         </div>
     )
 }
