ee911560fc96ab864252de59301bd1f167be04ee
Ritwik Srivastava <72033393+fritz-31@users.noreply.github.com>
2021-09-28 13:04:15 +0530

    fourth commit

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,16 +1,22 @@
+import{useEffect, useState} from "react";
 import "./feed.css"
 import Post from "../post/Post"
 import Share from "../share/Share"
-import{Posts} from "../../dummyData"
+import axios from "axios"
 
 export default function Feed() {
+    const[posts,setPosts]= useState([])
+    const[text,setText]=useState("");
+    useEffect(()=>{
+        axios.get("")
+    },[])
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share/>
-                {Posts.map((p)=>(
+                {/*Posts.map((p)=>(
                     <Post key={p.id} post={p}/>
-                ))}
+                ))*/}
             </div>
         </div>
     );
